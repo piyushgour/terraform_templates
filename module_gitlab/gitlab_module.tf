c7f7401287f49b5b180e828099823023870fa9bf
piyush gour <piyush.gour93@gmail.com>
2020-11-06 20:14:02 +0530

    add project module

---
+++ module_gitlab/gitlab_module.tf
@@ -0,0 +1,22 @@
+resource "gitlab_project" "project" {
+    name                                             = "${var.name}"
+    description                                      = "${var.DESCRIPTION}"
+    namespace_id                                     = "${var.NAMESPACE_ID}"
+    visibility_level                                 = "${var.VISIBILITY_LEVEL}"
+    default_branch                                   = "${var.DEFAULT_BRANCH}"
+    request_access_enabled                           = false
+    issues_enabled                                   = true
+    merge_requests_enabled                           = true
+    pipelines_enabled                                = true
+    approvals_before_merge                           = 1
+    wiki_enabled                                     = true
+    snippets_enabled                                 = true
+    container_registry_enabled                       = true
+    lfs_enabled                                      = true
+    merge_method                                     = "merge"
+    only_allow_merge_if_pipeline_succeeds            = true
+    only_allow_merge_if_all_discussions_are_resolved = true
+    shared_runners_enabled                           = true
+    initialize_with_readme                           = true
+
+}
---
+++ module_gitlab/variable.tf
@@ -0,0 +1,20 @@
+vatiable "name"{
+    type = string
+    default = ""
+     
+}
+
+vatiable "NAMESPACE_ID"{
+    type = number
+    default = "" 
+}
+
+vatiable "VISIBILITY_LEVEL"{
+    type = string
+    default = ""
+}
+
+vatiable "DEFAULT_BRANCH"{
+    type = string
+    default = ""
+}
