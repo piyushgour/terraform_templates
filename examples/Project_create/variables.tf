ec3ca6ed46c099d889dd1acdae8b7a737145cce6
piyush gour <piyush.gour93@gmail.com>
2020-11-09 09:21:19 +0530

    correct typo

---
+++ examples/Project_create/variables.tf
@@ -0,0 +1,26 @@
+variable "name"{
+    type = string
+    default = ""
+     
+}
+
+variable "DESCRIPTION"{
+    type = string
+    default = ""
+     
+}
+
+variable "NAMESPACE_ID"{
+    type = number
+    default = "" 
+}
+
+variable "VISIBILITY_LEVEL"{
+    type = string
+    default = ""
+}
+
+variable "DEFAULT_BRANCH"{
+    type = string
+    default = ""
+}
---
+++ modules/gitlab_project/variables.tf
@@ -0,0 +1,25 @@
+variable "name"{
+    type = string
+    default = ""
+     
+}
+
+variable "DESCRIPTION"{
+    type = string
+    default = ""     
+}
+
+variable "NAMESPACE_ID"{
+    type = number
+    default = "" 
+}
+
+variable "VISIBILITY_LEVEL"{
+    type = string
+    default = ""
+}
+
+variable "DEFAULT_BRANCH"{
+    type = string
+    default = ""
+}
